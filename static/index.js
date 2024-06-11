0ffd75111289b8463ec799429d47711ab4fb6c6a
root <root@AJ.>
2024-06-11 10:44:15 -0400

    added single page functionality

--- static/index.js
+++ static/index.js
@@ -1,1 +1,23 @@
+// Controls which div is being displayed
+function display_section(section) {
+    document.getElementById("default_div").style.display = 'none';
+    document.querySelectorAll(".section_display").forEach((section) => {
+        section.style.display = "none";
+    });
+    document.querySelector(`#${section}`).style.display = "block";
+}
 
+/* for each button with the "section_button" class,
+*  it calls the display_section function
+*/
+document.querySelectorAll(".section_button").forEach((button) => {
+    button.onclick = () => {
+        display_section(button.dataset.section);
+    }
+});
+
+// Video Conversion
+document.querySelector("#vid_convert_form").onsubmit = () => {
+    console.log("submiting form");
+    return false;
+}
